d57ad81050cb24dfa7fff0bdc3047944669d0c89
Ray Azrin Karim <rayazrin19@gmail.com>
2025-05-15 11:01:42 +0700

    add tailwind support

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,7 +1,9 @@
+import './styles.css';
+
 const App = () => {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1 className='text-5xl'>Hello, world!</h1>
       <p>This is a simple React app.</p>
     </div>
   );
